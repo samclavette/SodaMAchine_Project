24b8fd46266eac651cf15c84b7a560283c09356b
Sam Clavette <samclavette@yahoo.com>
2020-07-21 11:16:50 -0500

    I made Coin/Can classes abstract and made Penny/Nickle/Dime/Quarter/RootBeer/OrangeSoda/Cola classes inherited ones.

--- SodaMachine/Coin.cs
+++ SodaMachine/Coin.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SodaMachine
 {
-    class Coin
+    public abstract class Coin
     {
     }
 }
--- SodaMachine/Dime.cs
+++ SodaMachine/Dime.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SodaMachine
 {
-    class Dime
+    class Dime : Coin
     {
     }
 }
