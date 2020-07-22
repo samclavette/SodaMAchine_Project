50d49efe6305c564081348b75bf078a9600738c6
Sam Clavette <samclavette@yahoo.com>
2020-07-22 10:03:02 -0500

    I added methods to add each type of coin to the wallet and created the logic for those methods in the Wallet class.

--- SodaMachine/Wallet.cs
+++ SodaMachine/Wallet.cs
@@ -10,6 +10,49 @@ namespace SodaMachine
     {
         public Card card;
         public List<Coin> coins;
+        Quarter quarter;
+        Dime dime;
+        Nickel nickel;
+        Penny penny;
 
+        public void AddCoinsToWallet()
+        {
+            AddPenniesToWallet();
+            AddNickelsToWallet();
+            AddDimesToWallet();
+            AddQuartersToWallet();
+        }
+
+        public void AddPenniesToWallet()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                coins.Add(penny);
+            }
+        }
+
+        public void AddNickelsToWallet()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                coins.Add(nickel);
+            }
+        }
+
+        public void AddDimesToWallet()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                coins.Add(dime);
+            }
+        }
+
+        public void AddQuartersToWallet()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                coins.Add(quarter);
+            }
+        }
     }
 }
