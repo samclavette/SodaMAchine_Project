04bee143916de187f2b91744e42ca0821d2a9cd3
Sam Clavette <samclavette@yahoo.com>
2020-07-21 19:02:12 -0500

    I added 4 methods for adding each type of soda can to the SodaMachine class and completed the logic for every method in the class.

--- SodaMachine/SodaMachine.cs
+++ SodaMachine/SodaMachine.cs
@@ -15,16 +15,45 @@ namespace SodaMachine
         Dime dime;
         Nickel nickel;
         Penny penny;
+        RootBeer rootBeer;
+        OrangeSoda orangeSoda;
+        Cola cola;
 
         public SodaMachine()
         {
             StockRegister();
-            register.Add(quarter);
-            register.Add(dime);
-            register.Add(nickel);
-            register.Add(penny);
+            StockInventory();
+        }
+        private void StockInventory()
+        {
+            StockRootBeer();
+            StockOrangeSoda();
+            StockCola();
         }
 
+        private void StockRootBeer()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                inventory.Add(rootBeer);
+            }
+        }
+
+        private void StockOrangeSoda()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                inventory.Add(orangeSoda);
+            }
+        }
+
+        private void StockCola()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                inventory.Add(cola);
+            }
+        }
         private void StockRegister()
         {
             StockPennies();
@@ -35,22 +64,35 @@ namespace SodaMachine
 
         private void StockPennies()
         {
-
+            for (int i = 0; i < 50; i++)
+            {
+                register.Add(penny);
+            }
         }
 
         private void StockNickels()
         {
-
+            for (int i = 0; i < 20; i++)
+            {
+                register.Add(nickel);
+            }
         }
 
         private void StockDimes()
         {
-
+            for (int i = 0; i < 10; i++)
+            {
+                register.Add(dime);
+            }
         }
 
         private void StockQuarters()
         {
-
+            for (int i = 0; i < 20; i++)
+            {
+                register.Add(quarter);
+            }
+            
         }
 
 
